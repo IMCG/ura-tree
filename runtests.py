88570e287857f57c65f495e0569d9fbac5e73d6d
Nicolas Arciniega <nick7650@hotmail.com>
2014-05-25 22:55:39 -0400

    Add support to automatically run distinct dataset

--- runtests.py
+++ runtests.py
@@ -3,15 +3,19 @@ import sys
 import subprocess
 import os.path
 
-def checkfiles(fileroot, smallset, filetype, threadnum):
+def checkfiles(fileroot, set_type, filetype, threadnum):
     #Check existence of files
-    directory = ""
-    if smallset:
-        directory += "smallset/"
-    else:
-        directory += "largeset/"
+    directory = set_type + "/"
 
-    directory += filetype + str(threadnum).zfill(2)
+    if (set_type == 'distinctset'):
+        if (filetype == 'skew'):
+            directory += "rand"
+        else:
+            directory += "sort"
+    else:
+        directory += filetype
+    
+    directory += str(threadnum).zfill(2)
     dirExist = os.path.isdir(os.path.join(fileroot, directory))
     fileExist = True
 
@@ -26,21 +30,25 @@ def checkfiles(fileroot, smallset, filetype, threadnum):
 
     return fileExist and dirExist
 
-def buildcommand(noOptimistic, smallset, filetype, threadnum):
+def buildcommand(noOptimistic, set_type, filetype, threadnum):
     command = "./NewFosterbtreeg_URA 0 w"
 
     if(noOptimistic):
-        command = command + " 0"
+        command += " 0"
     else:
-        command = command + " 1"
+        command += " 1"
     
     for partnum in range(1, threadnum+1):
-        if(smallset):
-            command = command + " smallset/"
+        command += " " + set_type + "/"
+        if (set_type == 'distinctset'):
+            if (filetype == 'skew'):
+                command += "rand"
+            else:
+                command += "sort"
         else:
-            command = command + " largeset/"
+            command += filetype
 
-        command = command + filetype + str(threadnum).zfill(2) + "/part" + str(partnum).zfill(2)
+        command += str(threadnum).zfill(2) + "/part" + str(partnum).zfill(2)
 
     return command
 
@@ -51,7 +59,7 @@ parser.add_argument('-s', '--start', action='store', default=1, type=int, choice
 parser.add_argument('-e', '--end', action='store', default=32, type=int, choices=[1, 2, 4, 8, 16, 32], help='The ending number of threads. Pick 1, 2, 4, 8, 16, or 32')
 parser.add_argument('--no-sorted', action='store_true', help='Don\'t include sorted data sets')
 parser.add_argument('--no-optimistic', action='store_true', help='Don\'t use optimistic searching')
-parser.add_argument('--smallset', action='store_true', help='Use small dataset')
+parser.add_argument('-t', '--set-type', action='store', type=str, default='largeset', choices=['largeset', 'smallset', 'distinctset'], help='Select type of dataset')
 
 args = parser.parse_args()
 
@@ -68,21 +76,22 @@ FILE_ROOT = os.path.abspath(os.path.dirname(__file__))
 
 # Run tests for different threads
 while(thread <= endthread):
+    
     #Check existence of files
-    if(checkfiles(FILE_ROOT, args.smallset, "skew", thread)):
+    if(checkfiles(FILE_ROOT, args.set_type, "skew", thread)):
             #Execute command and write to file
-            #print buildcommand(args.no_optimistic, args.smallset, "skew", thread) 
+            #print buildcommand(args.no_optimistic, args.set_type, "skew", thread) 
             print "Running test for skew %(threadnum)d case." % {"threadnum": thread }
-            out.write(str(subprocess.check_output(buildcommand(args.no_optimistic, args.smallset, "skew", thread).split())))
+            out.write(str(subprocess.check_output(buildcommand(args.no_optimistic, args.set_type, "skew", thread).split())))
             
     
     # Check if we should also do sorted values
     if not(args.no_sorted):
-        if(checkfiles(FILE_ROOT, args.smallset, "sorted", thread)):
+        if(checkfiles(FILE_ROOT, args.set_type, "sorted", thread)):
             # Execute command and write to file
-            #print buildcommand(args.no_optimistic, args.smallset, "sorted", thread)
+            #print buildcommand(args.no_optimistic, args.set_type, "sorted", thread)
             print "Running test for sorted %(threadnum)d case." % {"threadnum": thread }
-            out.write(str(subprocess.check_output(buildcommand(args.no_optimistic, args.smallset, "sorted", thread).split())))
+            out.write(str(subprocess.check_output(buildcommand(args.no_optimistic, args.set_type, "sorted", thread).split())))
 
     thread *= 2
 
